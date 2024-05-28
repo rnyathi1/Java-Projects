94b53a42fdcac86c118ca98b28a9e1ec1dc27d07
rnyathi1 <rbnyathiwork@yahoo.com>
2024-05-28 18:07:02 +0100

    Update Main.java

--- BondPricer/src/com/company/Main.java
+++ BondPricer/src/com/company/Main.java
@@ -9,8 +9,9 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
-        frame.setSize(600,600);
+      
         frame.setVisible(true);
+        frame.setSize(600,600);
         frame.setResizable(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel BPP = new BPPanel(new GridLayout(10,2));
