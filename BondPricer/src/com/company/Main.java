7c060e4ae029bc14ff3566ec0d7d4e423979ebbf
rnyathi1 <rbnyathiwork@yahoo.com>
2024-05-28 11:37:08 +0100

    Inputs Added

--- BondPricer/src/com/company/Main.java
+++ BondPricer/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is the face value?");
+        int faceValue = scan.nextInt();
+        System.out.println("What is the Coupon Rate?");
+        int couponRate = scan.nextInt();
+        System.out.println("What is the amount of payments per year (1/2/4/12) ?");
+        int Frequency = scan.nextInt();
+        System.out.println("What is the Yield?");
+        int Yield = scan.nextInt();
+        System.out.println("How many years till maturity?");
+        int maturityYear = scan.nextInt();
+
+
+
     }
 }
