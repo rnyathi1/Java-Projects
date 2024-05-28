993482f465e9a36b4c4e2df0848260d9a1d9e0ac
rnyathi1 <rbnyathiwork@yahoo.com>
2024-05-28 18:07:53 +0100

    Update Main.java

--- BondPricer/src/com/company/Main.java
+++ BondPricer/src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
-      
+
         frame.setVisible(true);
         frame.setSize(600,600);
         frame.setResizable(true);
