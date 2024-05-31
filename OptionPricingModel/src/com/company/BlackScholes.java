0e9254f6af3451ce6e665ad11daef88b9c2b1b4f
rnyathi1 <rbnyathiwork@yahoo.com>
2024-05-31 15:43:58 +0100

    BlackScholes Calc Done

--- OptionPricingModel/src/com/company/BlackScholes.java
+++ OptionPricingModel/src/com/company/BlackScholes.java
@@ -14,10 +14,27 @@ public class BlackScholes extends JPanel {
         mButton.addActionListener(e -> main.switchToMenu());
         add(button);
         add(mButton);
+        System.out.println(BlackScholesCalc(105,100,1,0.05,0.2,0,false));
     }
 
-    public double BlackScholesCalc(double strikePrice, double stockPrice, double timeExpiration, double riskFreeRate, double volatility, double dividendYield){
-            
-        return 0;
+    public double BlackScholesCalc(double strikePrice, double stockPrice, double timeExpiration, double riskFreeRate, double volatility, double dividendYield , boolean call){
+        double d1 = 1/ (volatility * Math.sqrt(timeExpiration)) * (Math.log(stockPrice / strikePrice) + (riskFreeRate - dividendYield + 0.5*(Math.pow(volatility,2))*timeExpiration));
+        double d2 = d1 - (volatility * Math.sqrt(timeExpiration));
+        double callOption = (stockPrice * Math.exp(-1 * dividendYield * timeExpiration) * CNDF(d1)) - (strikePrice * Math.exp(-1 * riskFreeRate * timeExpiration) * CNDF(d2));
+        double putOption = (strikePrice * Math.exp(-1 * riskFreeRate * timeExpiration) * CNDF(-d2)) - (stockPrice * Math.exp(-1 * dividendYield * timeExpiration) * CNDF(-d1));
+        return call ? callOption : putOption;
+    }
+
+    double CNDF(double x)
+    {
+        int neg = (x < 0d) ? 1 : 0;
+        if ( neg == 1)
+            x *= -1d;
+
+        double k = (1d / ( 1d + 0.2316419 * x));
+        double y = (((( 1.330274429 * k - 1.821255978) * k + 1.781477937) * k - 0.356563782) * k + 0.319381530) * k;
+        y = 1.0 - 0.398942280401 * Math.exp(-0.5 * x * x) * y;
+
+        return (1d - neg) * y + neg * (1d - y);
     }
 }
