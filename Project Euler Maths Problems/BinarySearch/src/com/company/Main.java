f872cfe77023038b992557b42ec2e022ff1becc2
rnyathi1 <rbnyathiwork@yahoo.com>
2024-06-19 18:43:01 +0100

    Binary Search

--- Project Euler Maths Problems/BinarySearch/src/com/company/Main.java
+++ Project Euler Maths Problems/BinarySearch/src/com/company/Main.java
@@ -4,6 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] 
+        int[] arr = {0,1,1,2,3,4,5,6,7};
+        int number = 6;
+        int low = 0;
+        int high = arr.length;
+        System.out.println(binarySearch(arr,number,low,high));
+
     }
+
+    public static int binarySearch(int[] arr, int n, int low, int high){
+
+        if(high >= low){
+            int mid = low + (high - low) / 2;
+            System.out.println(mid);
+            if (arr[mid] == n) return mid;
+            if(n > mid){
+                return binarySearch(arr,n,mid+1,high);
+            }
+            return binarySearch(arr,n,low,mid -1 );
+
+        }
+
+        return -1;
+    }
+
 }
