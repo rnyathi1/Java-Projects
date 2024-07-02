dcc82e805a92f3cf3233e12e3956212dbab52688
rnyathi1 <rbnyathiwork@yahoo.com>
2024-07-02 14:20:39 +0100

    LC

--- LC/problem1071/src/com/company/Main.java
+++ LC/problem1071/src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// LeetCode Problem 1071
+
+        String str1 = "ABCABC", str2 = "ABC";
+
+        if(!(str1 + str2).equals(str2+str1)){
+            System.out.println("");
+        }
+        System.out.println(str1.substring(0,gcd(str1.length(),str2.length())));
     }
+
+    static int gcd(int a,int b){
+
+        return b == 0 ? a: gcd(b, a % b);
+     }
+
+
 }
--- LC/problem1768/src/com/company/Main.java
+++ LC/problem1768/src/com/company/Main.java
@@ -3,30 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-/*
-        Example 1:
-        Input: word1 = "abc", word2 = "pqr"
-        Output: "apbqcr"
-        Explanation: The merged string will be merged as so:
-        word1:  a   b   c
-        word2:    p   q   r
-        merged: a p b q c r
-        Example 2:
-        Input: word1 = "ab", word2 = "pqrs"
-        Output: "apbqrs"
-        Explanation: Notice that as word2 is longer, "rs" is appended to the end.
-                word1:  a   b
-        word2:    p   q   r   s
-        merged: a p b q   r   s
-        Example 3:
-
-        Input: word1 = "abcd", word2 = "pq"
-        Output: "apbqcd"
-        Explanation: Notice that as word1 is longer, "cd" is appended to the end.
-                word1:  a   b   c   d
-        word2:    p   q
-        merged: a p b q c   d
-        */
+    //Leetcode 1768
 
         int j = 0,k = 0,i = 0;
         String word1 = "abcd";
