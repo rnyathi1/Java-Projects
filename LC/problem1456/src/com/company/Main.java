b95788d02c0b0d8fd76fc13576a7f5f1d63b42cc
rnyathi1 <rbnyathiwork@yahoo.com>
2024-08-20 09:00:54 +0100

    gb

--- LC/problem1456/src/com/company/Main.java
+++ LC/problem1456/src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         String s = "abciiidef"; int k = 3;
-        int i = 0; int j = k -1; int count = 0;
+        int i = 0; int j = k -1; int count = 0; int maxC = Integer.MIN_VALUE;
         HashSet<Character> map = new HashSet<Character>();
         map.add('a');
         map.add('e');
@@ -16,10 +16,17 @@ public class Main {
         for(int y = 0; y < k; y++){
             if(map.contains(s.charAt(y))) count++;
         }
+        maxC = Math.max(count, maxC);
         while(j != s.length()){
-            
+
+            if(map.contains(s.charAt(i))) count--;
+            j++;
+            i++;
+
+            if(j!= s.length()) if(map.contains(s.charAt(j)))count++;
+            maxC = Math.max(count, maxC);
         }
 
-
+        System.out.println(maxC);
     }
 }
